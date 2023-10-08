7874cceea0db80cedb24c2908d6caf177ee1287a
Umang Yadav <125858561+CoderUmangRao@users.noreply.github.com>
2023-10-08 05:39:30 +0000

    const or let or var ki khani

---
+++ 01-basics/02-variable/variable.js
@@ -0,0 +1,6 @@
+const accountId= 12345
+let accountName = "Umang"
+var accountEmail = "yadav@gmail.com"
+address = "hapur"
+
+console.log(accountId);
---
+++ 02-variable/variable.js
@@ -0,0 +1,17 @@
+const accountId= 12345 // can not change value by const variable
+let accountName = "Umang"  // can not make a same name by let variable
+let accountName1 = "Rao"
+var accountEmail = "yadav@gmail.com"  // make a same name by var variable and changes globally
+var accountEmail = "rao@gmail.com"
+address = "hapur"
+
+{
+    // {} is called scope
+}
+
+accountEmail = "umang@gmail.com"
+accountEmail = 'yadavji@gmail.com'
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountEmail);
+console.table([accountId,accountName,accountEmail,accountName1]);   /// by using table the all variable declare in one console
