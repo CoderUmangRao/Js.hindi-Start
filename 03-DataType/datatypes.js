bd5762721c1068f553d2582499ff6cc030ac98fa
Umang Yadav <125858561+CoderUmangRao@users.noreply.github.com>
2023-10-08 07:25:37 +0000

    typeof variable

---
+++ 03-DataType/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; /// treat all js code as newer version
+// alert("hello")  /// we are using node js not browser
+
+//primitive data type or non-primitive data type
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+let name = "Umang"
+let userId = 125434
+let anotherId = "umang1234"
+
+let score= null
+
+console.log(typeof "Umang");
+console.log(typeof userId);
+console.log(typeof anotherId);
+let anotherId2 = Number (anotherId)
+console.log(typeof anotherId2);
+
+console.log(anotherId2);
+console.log(typeof score);
