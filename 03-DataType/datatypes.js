cbed52d92d8c579dca6578df19e34c1587cf8ca4
Umang Yadav <125858561+CoderUmangRao@users.noreply.github.com>
2023-10-21 05:52:34 +0000

    types of data in js

--- 03-DataType/datatypes.js
+++ 03-DataType/datatypes.js
@@ -25,3 +25,49 @@ console.log(typeof anotherId2);
 
 console.log(anotherId2);
 console.log(typeof score);
+
+// let score = undefined
+
+// console.log(typeof score);
+// let valueInScore = Number(score)
+// console.log(typeof valueInScore);
+// console.log(valueInScore);
+
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1  false => 0
+
+// let isLoggedIn = "Umang"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+    // ********************** Operations *************//
+
+// let value = 5
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "hello"
+// let str2 =" Umang"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log(1+2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "3");
+// console.log("1" + 2 + 3);
+
+// let num1, num2, num3
+// num1=num2=num3=2+2
+// console.log(num2);
+
+let number = 1
+++number
+console.log(number);
